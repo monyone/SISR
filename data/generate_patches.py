566c1c3d0ea746aeefb918d66ed03364ba95867b
monyone <monyone.teihen@gmail.com>
2023-06-24 16:38:57 +0900

    prepare T91 dataset

--- data/generate_patches.py
+++ data/generate_patches.py
@@ -11,7 +11,8 @@ from glob import glob
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description='PyTorch SISR (Single Image Super Resolution)')
-  parser.add_argument('--crop', type=int, default=128, help='crop size')
+  parser.add_argument('--crop', type=int, default=32, help='crop size')
+  parser.add_argument('--stride', type=int, default=14, help='crop size')
   parser.add_argument('--input_path', type=Path, required=True, help="input path")
   parser.add_argument('--output_dir', type=Path, required=True, help="output path")
 
@@ -22,8 +23,8 @@ if __name__ == '__main__':
     path = Path(path_str)
     img = io.read_image(path_str, mode=io.ImageReadMode.RGB) / 255
     _, w, h = img.size()
-    for top in range(0, h - (h % args.crop), args.crop):
-      for left in range(0, w - (w % args.crop), args.crop):
+    for top in range(0, h - args.crop + 1, args.stride):
+      for left in range(0, w - args.crop + 1, args.stride):
         bottom = min(h, top + args.crop)
         right = min(w, left + args.crop)
         utils.save_image(F.crop(img, top=top, left=left, width=args.crop, height=args.crop), str(args.output_dir / f'{path.stem}_{top}_{left}{path.suffix}'), nrow=1)
